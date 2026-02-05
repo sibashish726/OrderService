65af09a9a739d7b53455c9b2ad14a42b2888fade
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2026-02-05 16:11:17 +0530

    first commit

---
+++ src/main/java/com/example/product/service/OrderService.java
@@ -0,0 +1,5 @@
+package com.example.product.service;
+
+public interface OrderService {
+
+}
---
+++ src/main/java/com/example/product/service/OrderServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.product.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class OrderServiceImpl implements OrderService {
+
+}
