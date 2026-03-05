9402ab30e44b38c371bd402ab744960a8a30ddc2
sibashish726 <sibashish.biswas@lumen.com>
2026-03-05 11:29:07 +0530

    Implementing Circuit Breaker in Order Service

--- src/main/java/com/example/product/external/client/PaymentService.java
+++ src/main/java/com/example/product/external/client/PaymentService.java
@@ -5,10 +5,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.example.product.exception.CustomException;
 import com.example.product.external.request.PaymentRequest;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@CircuitBreaker(name= "external", fallbackMethod = "fallback")
 @FeignClient(name="PAYMENT-SERVICE/payment")
 public interface PaymentService {
 	 @PostMapping("/doPayment")
 	 public ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+	 default void fallback(Exception e) {
+		 throw new CustomException("Payment service is not available", "UNAVAILABLE", 5000);
+	 }
 }
--- src/main/java/com/example/product/external/client/ProductService.java
+++ src/main/java/com/example/product/external/client/ProductService.java
@@ -5,9 +5,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.example.product.exception.CustomException;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+@CircuitBreaker(name= "external", fallbackMethod = "fallback")
 @FeignClient(name="PRODUCT-SERVICE/product")
 public interface ProductService {
 	@PutMapping("/reduceQuantity/{id}")
 	ResponseEntity<Void> reduceQuantity(@PathVariable("id") long productId, @RequestParam long quantity);
+	
+	
+	default void fallback(Exception e) {
+		 throw new CustomException("Product service is not available", "UNAVAILABLE", 5000);
+	 }
 }
